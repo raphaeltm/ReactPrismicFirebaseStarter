8b66fda138301c6c1390c42f92d819dceeb25c74
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2017-09-13 13:48:30 -0700

    added bulma and restructured homepage.

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,11 +4,32 @@ import {RichText} from "prismic-reactjs";
 class Homepage extends React.Component {
   render() {
     let page = this.props.content.data;
-    return <article>
-      <header>
-        {page.title && RichText.render(page.title)}
-      </header>
-    </article>
+    return <div className="section" id="home">
+      <div className="container">
+        <div className="columns">
+          <article className="column">
+            <header>
+              {page.title && RichText.render(page.title)}
+            </header>
+            <hr/>
+            <div>
+              {page.content && RichText.render(page.content)}
+            </div>
+          </article>
+          <aside className="column">
+            <header>
+              {page.aside_title && RichText.render(page.aside_title)}
+            </header>
+            <hr/>
+            {page.aside_content && RichText.render(page.aside_content)}
+          </aside>
+        </div>
+        <hr/>
+        <footer className="has-text-centered">
+          {page.footer && RichText.render(page.footer)}
+        </footer>
+      </div>
+    </div>;
   }
 }
 
